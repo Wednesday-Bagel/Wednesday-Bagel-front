ceab8518fe108e44185d5f0b6f4e820bd243107b
seonnn <ssni0105@gmail.com>
2022-04-27 21:14:15 +0900

    footer 추가

--- src/pages/TeamBoardPage.tsx
+++ src/pages/TeamBoardPage.tsx
@@ -96,6 +96,19 @@ export function TeamBoardPage() {
               </button>
             </div>
           </div>
+          <footer className="text-gray-400 flex flex-col justify-center items-center">
+            <button
+              onClick={() =>
+                window.open(
+                  "https://www.freepik.com/vectors/portrait-illustration"
+                )
+              }
+            >
+              "https://www.freepik.com/vectors/portrait-illustration"
+            </button>
+            Portrait illustration vector created by pikisuperstar -
+            www.freepik.com
+          </footer>
         </div>
       </div>
     );
